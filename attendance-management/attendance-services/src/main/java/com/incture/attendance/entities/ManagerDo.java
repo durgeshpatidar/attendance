23a032fb36e4ecfbf03e823772e10aa8415bd825
hp <hp@192.168.0.31>
2020-07-05 00:43:18 +0530

    ManagerDo updated

---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/entities/ManagerDo.java
@@ -0,0 +1,53 @@
+package com.incture.attendance.entities;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+
+@Entity
+@Table(name = "MANAGER")
+public class ManagerDo {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "ID")
+	private String id;
+	
+	@OneToOne
+	@JoinColumn(name="EMPLOYEE_ID")
+	private EmployeeDo employee;
+	
+	@OneToOne
+	@JoinColumn(name="MANAGER_ID")
+	private EmployeeDo employee1;
+	
+	@Column
+	@CreationTimestamp
+	private Date start_date;
+	
+	
+	@Column
+	@UpdateTimestamp
+	private Date end_date;
+	
+	
+	
+	
+}
