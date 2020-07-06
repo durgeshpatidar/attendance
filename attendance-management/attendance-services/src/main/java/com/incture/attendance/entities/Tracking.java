0fee4ed65e56b554b27fdd02d6acbdbe1117c4eb
durgeshpatidar <durgeshpatidar4@gmail.com>
2020-07-06 17:10:44 +0530

    update tracking class

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/entities/Tracking.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/entities/Tracking.java
@@ -1,4 +1,26 @@
-class Tracking
+package com.incture.attendance.entities;
+
+
+@Entity
+@Table(name = "TRACKING")
+class Tracking implements BaseDo
 {
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "ID")
+	private String id;
+	
+	@ManyToOne
+	@JoinColumn(name="EMPLOYEE_ID")
+	private EmployeeDo employee;
+	
+	@ManyToOne
+	@JoinColumn(name="ADDRESS_ID")
+	private EmployeeDo address;
+	
+	@Column(name = "DATE", columnDefinition = "DATE")
+	private Date date;
+	
 	
 }
