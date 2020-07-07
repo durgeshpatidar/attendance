3abd22638af13d0898f66db8a070d81203654720
hp <hp@192.168.0.31>
2020-07-08 01:12:41 +0530

    tracking updated

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/TrackingDaoImpl.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/TrackingDaoImpl.java
@@ -9,32 +9,51 @@ import com.incture.attendance.entities.AddressDo;
 import com.incture.attendance.entities.EmployeeDo;
 import com.incture.attendance.entities.TrackingDo;
 
+
 @Repository("TrackingDaoImpl")
 public class TrackingDaoImpl extends BaseDao<TrackingDo, TrackingDto> implements TrackingDao {
 
+	Session session = getSession();
+
+	@Override
+	protected TrackingDo importDto(TrackingDto trackingDto) {
+		TrackingDo entity= null;
+		entity.setId(trackingDto.getId());
+		entity.setAddressTracking(session.get(AddressDo.class, trackingDto.getAddressId()));
+		entity.setEmpTracking(session.get(EmployeeDo.class, trackingDto.getEmpId()));
+		entity.setCheckIn(trackingDto.getCheckIn());
+		entity.setCheckOut(trackingDto.getCheckIn());
+		entity.setDate(trackingDto.getDate());
+		entity.setTotalHours(trackingDto.getTotalHours());
+		return entity;
+		
+		}
+	
+
+	@Override
+	protected TrackingDto exportDto(TrackingDo entity) {
+		TrackingDto dto = null;
+		dto.setId(entity.getId());
+		dto.setAddressId(entity.getAddressTracking().getId());
+		dto.setEmpId(entity.getEmpTracking().getId());
+		dto.setCheckIn(entity.getCheckIn());
+		dto.setCheckOut(entity.getCheckOut());
+		dto.setTotalHours(entity.getTotalHours());
+		dto.setDate(entity.getDate());
+		return dto;
+	}
+	//For adding tracking
 	@Override
 	public void addTracking(TrackingDto trackingdto) {
-		// TODO Auto-generated method stub
+		getSession().save(importDto(trackingdto));
+		
 		
 	}
 
 	@Override
 	public void updateTracking(TrackingDto trackingdto) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
-	@Override
-	protected TrackingDo importDto(TrackingDto fromDto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected TrackingDto exportDto(TrackingDo entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
 }
