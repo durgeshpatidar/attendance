1af007b42f3e53548d3ecb2012bdaee8404691c2
hp <hp@192.168.0.31>
2020-07-07 09:15:39 +0530

    address,designation,employee and manager master do updated

---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/entities/AddressMasterDo.java
@@ -0,0 +1,125 @@
+//Address Master Class
+package com.incture.attendance.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "ADDRESS")
+@Data
+public class AddressMasterDo {
+private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "ID", columnDefinition = "NVARCHAR(36)")
+	private String id;
+	
+	@Column(name = "ADDRESS", columnDefinition = "VARCHAR(100)")
+	private String address;
+	
+	@Column(name = "CITY", columnDefinition = "VARCHAR(100)")
+	private String city;
+	
+	@Column(name = "STATE", columnDefinition = "VARCHAR(100)")
+	private String state;
+	
+	@Column(name = "PIN_CODE", columnDefinition = "NVARCHAR(10)")
+	private String pincode;
+	
+	@Column(name = "LOCATION_LAT", columnDefinition = "DOUBLE")
+	private Double locationLat;
+	
+	@Column(name = "LOCATION_LON", columnDefinition = "DOUBLE")
+	private Double locationLon;
+	
+	//Constructor
+	public AddressMasterDo() {
+		super();
+	}
+
+	public AddressMasterDo(String id, String address, String city, String state, String pincode, Double locationLat,
+			Double locationLon) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+		this.locationLat = locationLat;
+		this.locationLon = locationLon;
+	}
+	
+	//Getters and Setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	public Double getLocationLat() {
+		return locationLat;
+	}
+
+	public void setLocationLat(Double locationLat) {
+		this.locationLat = locationLat;
+	}
+
+	public Double getLocationLon() {
+		return locationLon;
+	}
+
+	public void setLocationLon(Double locationLon) {
+		this.locationLon = locationLon;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+
+}
---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/entities/ManagerMasterDo.java
@@ -0,0 +1,104 @@
+//Manager Master Class
+package com.incture.attendance.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MANAGER_MASTER")
+public class ManagerMasterDo {
+	@Id
+	@Column(name = "ID", columnDefinition = "NVARCHAR(36)")
+	private String id;
+	
+	@Column(name = "EMPLOYEE_ID", columnDefinition = "NVARCHAR(36)")
+	private String employeeId;
+	
+	@Column(name = "MANAGER_ID", columnDefinition = "NVARCHAR(36)")
+	private String managerId;
+	
+	@Column(name = "START_DATE", columnDefinition = "DATE")
+	private String startDate;
+	
+	@Column(name = "END_DATE", columnDefinition = "DATE")
+	private String endDate;
+	
+	@Column(name = "MANAGER_TYPE", columnDefinition = "NVARCHAR(20)")
+	private String managerType;
+	
+	//Constructor
+	public ManagerMasterDo() {
+		super();
+		
+	}
+	
+	public ManagerMasterDo(String id, String employeeId, String managerId, String startDate, String endDate,
+			String managerType) {
+		super();
+		this.id = id;
+		this.employeeId = employeeId;
+		this.managerId = managerId;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.managerType = managerType;
+	}
+	
+	//Getters and Setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(String managerId) {
+		this.managerId = managerId;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getManagerType() {
+		return managerType;
+	}
+
+	public void setManagerType(String managerType) {
+		this.managerType = managerType;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
