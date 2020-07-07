54ef040457a74e4a290974ba9e4a0b3d1990e67e
hp <hp@192.168.0.31>
2020-07-07 22:58:18 +0530

    tracking service implementation annotation added

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/service/TrackingServiceImpl.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/service/TrackingServiceImpl.java
@@ -1,5 +1,11 @@
 package com.incture.attendance.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class TrackingServiceImpl implements TrackingService {
 
 }
