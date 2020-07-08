e2024c9ae0de1f49982ec31357f3c0dfbdbeefc0
Gagandeep kaur <Gagandeep kaur@DESKTOP-7VTFF0N>
2020-07-08 12:21:41 +0530

    workflowtaskDto

---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dto/WorkflowTaskDto.java
@@ -0,0 +1,73 @@
+package com.incture.attendance.dto;
+
+import java.util.Date;
+
+public class WorkflowTaskDto {
+	private String id;
+	private String description;
+	private String empId;
+	private String managerId;
+	private Date requestdate;
+	private String comment;
+	private String status;
+	
+	//Constructors
+	
+	public WorkflowTaskDto() {
+		super();
+
+	}
+	public WorkflowTaskDto(String id, String description, String empId, String mangerId, Date requestdate, String  comment, String status, String managerId) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.empId = empId;
+		this.managerId = managerId;
+		this.requestdate = requestdate;
+		this.comment = comment;
+		this.status = status;
+	}
+	
+  //getters and setters
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getManagerId() {
+		return managerId;
+	}
+	public void setManagerId(String managerId) {
+		this.managerId = managerId;
+	}
+	public Date getRequestdate() {
+		return requestdate;
+	}
+	public void setRequestdate(Date requestdate) {
+		this.requestdate = requestdate;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
