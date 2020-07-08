8f2e3f97699910e646e814eaeac3566449d5df72
Gagandeep kaur <Gagandeep kaur@DESKTOP-7VTFF0N>
2020-07-08 13:33:17 +0530

    workflowtaskdao

---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/WorkflowTaskDao.java
@@ -0,0 +1,10 @@
+package com.incture.attendance.dao;
+
+import com.incture.attendance.dto.WorkflowTaskDto;
+
+public interface WorkflowTaskDao {
+	void addworkflowtask(WorkflowTaskDto workflowtaskdto);
+
+	void updateworklfowtask(WorkflowTaskDto workflowtaskdto);
+
+}
