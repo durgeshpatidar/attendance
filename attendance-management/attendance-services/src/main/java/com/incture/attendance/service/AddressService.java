1ce164bf8edc5a2b54ecfcbadd1d0ad17dd6daf1
durgeshpatidar <durgeshpatidar4@gmail.com>
2020-07-09 14:36:26 +0530

    address service added

---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.incture.attendance.service;
+
+
+import com.incture.attendance.dto.AddressDto;
+import com.incture.attendance.utils.ResponseDto;
+
+public interface AddressService {
+	ResponseDto addAddress(AddressDto addressDto);
+}
---
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/service/AddressServiceImpl.java
@@ -0,0 +1,14 @@
+package com.incture.attendance.service;
+
+import com.incture.attendance.dto.AddressDto;
+import com.incture.attendance.utils.ResponseDto;
+
+public class AddressServiceImpl implements AddressService {
+
+	@Override
+	public ResponseDto addAddress(AddressDto addressDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
