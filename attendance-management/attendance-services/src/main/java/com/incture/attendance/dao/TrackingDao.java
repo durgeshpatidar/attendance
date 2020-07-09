65230afc073fd71bab008f39fb030484560286e4
durgeshpatidar <durgeshpatidar4@gmail.com>
2020-07-09 22:46:34 +0530

    trackingDao updated

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/TrackingDao.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/TrackingDao.java
@@ -1,11 +1,17 @@
 package com.incture.attendance.dao;
 
+import java.util.List;
+
+import com.incture.attendance.dto.EmployeeDto;
+import com.incture.attendance.dto.TrackingDetailsDto;
 import com.incture.attendance.dto.TrackingDto;
 
 public interface TrackingDao {
 	void addTracking(TrackingDto trackingdto);
 
 	void updateTracking(TrackingDto trackingdto);
-	//
+	
+	List<TrackingDetailsDto> getTrackingDetails(EmployeeDto employeeDto);
+	
 
 }
