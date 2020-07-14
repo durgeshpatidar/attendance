4949ccc4a4d32718326dedaa51ad172c6db294d9
hp <hp@192.168.0.28>
2020-07-14 08:37:18 +0530

    profiledto updated

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/dto/ProfileDto.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dto/ProfileDto.java
@@ -13,14 +13,16 @@ public class ProfileDto {
 	private String emailId;
 	private String phoneNo;
 	private String profileImg;
-
+	private String designation;
+	
 	// Constructor
+	
 	public ProfileDto() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-
 	public ProfileDto(String id, String firstName, String lastName, String gender, Date birthDate, String bloodGroup,
-			String emailId, String phoneNo, String profileImg) {
+			String emailId, String phoneNo, String profileImg, String designation) {
 		super();
 		this.id = id;
 		this.firstName = firstName;
@@ -31,79 +33,71 @@ public class ProfileDto {
 		this.emailId = emailId;
 		this.phoneNo = phoneNo;
 		this.profileImg = profileImg;
+		this.designation = designation;
 	}
-
+	
 	// Getter and Setters
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return LastName;
 	}
-
 	public void setLastName(String lastName) {
 		LastName = lastName;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public Date getBirthDate() {
 		return birthDate;
 	}
-
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
-
 	public String getBloodGroup() {
 		return bloodGroup;
 	}
-
 	public void setBloodGroup(String bloodGroup) {
 		this.bloodGroup = bloodGroup;
 	}
-
 	public String getEmailId() {
 		return emailId;
 	}
-
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-
 	public String getPhoneNo() {
 		return phoneNo;
 	}
-
 	public void setPhoneNo(String phoneNo) {
 		this.phoneNo = phoneNo;
 	}
-
 	public String getProfileImg() {
 		return profileImg;
 	}
-
 	public void setProfileImg(String profileImg) {
 		this.profileImg = profileImg;
 	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
 
+	
+	
 }
