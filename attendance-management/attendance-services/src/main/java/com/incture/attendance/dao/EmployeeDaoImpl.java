786138f00a49513535b78fa57527cb11715b9581
durgeshpatidar <durgeshpatidar4@gmail.com>
2020-07-18 16:26:19 +0530

    update

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/EmployeeDaoImpl.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/EmployeeDaoImpl.java
@@ -7,7 +7,6 @@ import java.util.Random;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
