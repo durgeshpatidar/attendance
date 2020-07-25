26a771d3f1b0d75ecbfb916f2a10505020ebeccd
hp <hp@192.168.0.30>
2020-07-25 13:17:26 +0530

    hql in tracking

--- attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/TrackingDaoImpl.java
+++ attendance-management/attendance-services/src/main/java/com/incture/attendance/dao/TrackingDaoImpl.java
@@ -3,8 +3,11 @@ package com.incture.attendance.dao;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Query;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Order;
@@ -51,14 +54,21 @@ public class TrackingDaoImpl extends BaseDao<TrackingDo, TrackingDto> implements
 	// For CheckIn
 	@Override
 	public String addTracking(TrackingDto trackingdto) {
-		@SuppressWarnings("deprecation")
-		Criteria criteria = getSession().createCriteria(TrackingDo.class);
-		criteria.add(Restrictions.eq("date", trackingdto.getDate()));
-		criteria.add(Restrictions.eq("employee", getSession().get(EmployeeDo.class, trackingdto.getEmpId())));
-
-		TrackingDo track = (TrackingDo) criteria.uniqueResult();
+		//@SuppressWarnings("deprecation")
+		//Criteria criteria = getSession().createCriteria(TrackingDo.class);
+		//criteria.add(Restrictions.eq("date", trackingdto.getDate()));
+		//criteria.add(Restrictions.eq("employee", getSession().get(EmployeeDo.class, trackingdto.getEmpId())));
+		
+		//checking whether employee already checked in
+		String hql = "from TrackingDo where date = :date1 and employee = :employee";
+		Query query=getSession().createQuery(hql);
+		query.setParameter("date1", trackingdto.getDate());
+		query.setParameter("employee", getSession().get(EmployeeDo.class, trackingdto.getEmpId()));
+		query.executeUpdate();
+		TrackingDo track = (TrackingDo) query.getSingleResult();
 		if (track != null)
 			return null;
+		//If not checkedin 
 		trackingdto.setStatus("Pending");
 		TrackingDo tdo = importDto(trackingdto);
 		getSession().save(tdo);
@@ -67,43 +77,55 @@ public class TrackingDaoImpl extends BaseDao<TrackingDo, TrackingDto> implements
 	}
 
 	// For getting tracking details
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<TrackingDetailsDto> getTrackingDetails(String id, Date start, Date end) {
 		// Getting tracking details in between start and end date
-		@SuppressWarnings("deprecation")
-		Criteria criteria = getSession().createCriteria(TrackingDo.class);
-		criteria.add(Restrictions.eq("employee", getSession().get(EmployeeDo.class, id)));
+		//@SuppressWarnings("deprecation")
+		//Criteria criteria = getSession().createCriteria(TrackingDo.class);
+		//criteria.add(Restrictions.eq("employee", getSession().get(EmployeeDo.class, id)));
+		List<TrackingDo> tracks  =  new ArrayList<>(); ;
 		if (start != null && end != null) {
-
-			criteria.add(Restrictions.between("date", start, end));
-			criteria.setMaxResults(30);
+			
+			String hql = "from TrackingDo where employee = :employee and date>= :start and date<= :end order by date desc";
+			Query query=getSession().createQuery(hql);
+			query.setParameter("employee", getSession().get(EmployeeDo.class, id));
+			query.setParameter("start", start);
+			query.setParameter("end", end);
+			query.setMaxResults(30);
+			query.executeUpdate();
+		    tracks.addAll((Collection<? extends TrackingDo>) query.getResultList());
+			//criteria.add(Restrictions.between("date", start, end));
+			//criteria.setMaxResults(30);
 
 		}
-		if (start == null && end != null) {
-
-			criteria.add(Restrictions.le("date", end));
-			criteria.setMaxResults(30);
-
-		}
-		if (start != null && end == null) {
-			criteria.add(Restrictions.ge("date", start));
-			criteria.setMaxResults(30);
-		}
+		
 		if (start == null && end == null) {
-			criteria.setMaxResults(7);
+			String hql = "from TrackingDo where employee = :employee order by date desc";
+			Query query=getSession().createQuery(hql);
+			query.setParameter("employee", getSession().get(EmployeeDo.class, id));
+			query.setMaxResults(7);
+			query.executeUpdate();
+			tracks.addAll((Collection<? extends TrackingDo>) query.getResultList());
+			//criteria.setMaxResults(7);
 		}
-		criteria.addOrder(Order.desc("date"));
+		//criteria.addOrder(Order.desc("date"));
 
-		@SuppressWarnings("unchecked")
-		List<TrackingDo> trackings = criteria.list();
+		
 		// Getting employee name
-		@SuppressWarnings("deprecation")
-		Criteria crit = getSession().createCriteria(EmployeeMasterDo.class);
-		crit.add(Restrictions.eq("id", id));
-		EmployeeMasterDo emp = (EmployeeMasterDo) crit.uniqueResult();
+		//@SuppressWarnings("deprecation")
+		//Criteria crit = getSession().createCriteria(EmployeeMasterDo.class);
+		//crit.add(Restrictions.eq("id", id));
+		//EmployeeMasterDo emp = (EmployeeMasterDo) crit.uniqueResult();
+		String hql2 = "from EmployeeMasterDo where id :id";
+		Query query2=getSession().createQuery(hql2);
+		query2.setParameter("id", id);
+		EmployeeMasterDo emp = (EmployeeMasterDo) query2.getSingleResult();
+		
+		
 		List<TrackingDetailsDto> history = new ArrayList<>();
 
-		for (TrackingDo t : trackings) {
+		for (TrackingDo t : tracks) {
 
 			TrackingDetailsDto newTracking = new TrackingDetailsDto();
 			newTracking.setEmpId(id);
@@ -123,10 +145,15 @@ public class TrackingDaoImpl extends BaseDao<TrackingDo, TrackingDto> implements
 	@Override
 	public void updateTracking(String id, Date checkOut, double totalHours) {
 
-		@SuppressWarnings("deprecation")
-		Criteria criteria = getSession().createCriteria(TrackingDo.class);
-		criteria.add(Restrictions.eq("id", id));
-		TrackingDo current = (TrackingDo) criteria.uniqueResult();
+		//@SuppressWarnings("deprecation")
+		//Criteria criteria = getSession().createCriteria(TrackingDo.class);
+		//criteria.add(Restrictions.eq("id", id));
+		//TrackingDo current = (TrackingDo) criteria.uniqueResult();
+		
+		String hql = "from TrackingDo where id :id";
+		Query query=getSession().createQuery(hql);
+		query.setParameter("id", id);
+		TrackingDo current = (TrackingDo) query.getSingleResult();
 		current.setCheckOut(checkOut);
 		current.setTotalHours(totalHours);
 		if (totalHours >= 7.0 && totalHours <= 9.0) {
